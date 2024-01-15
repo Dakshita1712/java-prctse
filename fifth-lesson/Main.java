399cab0077adcf34a6706bf3a370b097b56dd8e7
Dakshita1712 <dakshita17dwivedi@gmail.com>
2024-01-15 18:52:45 +0530

    revised for loop

---
+++ fifth-lesson/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        String[] sweets = new String[4];
+        sweets[0] = "besan laddoo";
+        sweets[1] = "boondi laddoo";
+        sweets[2] = "kaju barfi";
+        sweets[3] = "khoya barfi";
+        for (int i = 0; i <= 3; i++) {
+            System.out.println(sweets[i]);
+        }
+
+        for (int i = 1; i <= 20; i = i * 2) {
+            System.out.println(i);
+        }
+
+    }
+
+}
